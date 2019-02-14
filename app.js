d3b52a8747d32c34051c5e8a3a7e822dcd96044c
CJ Horlebein <chrishorlebein@gmail.com>
2019-02-14 15:29:00 -0500

    backend template added

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+// GET method route
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
